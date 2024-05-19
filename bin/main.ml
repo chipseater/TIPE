984fddeb516750982da3053dbd46d596a7c5c9e6
Eric de Malet <jeanpauldemalet@yahoo.fr>
2024-05-19 18:27:16 +0200

    [Interpolation] Created an interpolation function for a 1-d data set

--- bin/main.ml
+++ bin/main.ml
@@ -3,5 +3,5 @@ open Mapgen;;
 let random_map = gen_random_values 50;;
 print_int_map random_map;;
 print_char '\n';;
-print_int_map (interpolate random_map);;
+print_int_map (average_map random_map);;
 
