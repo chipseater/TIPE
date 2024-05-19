4ea501175a4e652270f76a6ee4ca0d338bc542f4
Eric de Malet <jeanpauldemalet@yahoo.fr>
2024-05-19 15:39:53 +0200

    [Visualization] Pyenv and jupyter environement setup

--- bin/main.ml
+++ bin/main.ml
@@ -1,5 +1,7 @@
 open Mapgen;;
 
-let random_map = gen_random_values 5;;
-print_int_map (random_map);;
-print_int (average_adjacent random_map 1 1);;
+let random_map = gen_random_values 50;;
+print_int_map random_map;;
+print_char '\n';;
+print_int_map (interpolate random_map);;
+
