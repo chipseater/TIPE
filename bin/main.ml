e6b6412fb8f872c8cb001b0e4f5936b1ba76aa8b
Eric de Malet <jeanpauldemalet@yahoo.fr>
2024-05-24 16:14:34 +0200

    [Mapgen] Added layers to perlin noise

--- bin/main.ml
+++ bin/main.ml
@@ -2,7 +2,7 @@
 =======
 open Mapgen;;
 
-let random_map = perlin_map 1000 500 1;;
-print_float_map (random_map);;
+let random_map = perlin_map 500 200 4;;
+print_float_map random_map;;
 
 >>>>>>> dec9dce ([Mapgen] Perlin noise finaly works)
