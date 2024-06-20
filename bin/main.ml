2a6b6c0bd17399df120867ed0ffcf82f57f4175e
Eric de Malet <jeanpauldemalet@yahoo.fr>
2024-06-20 10:01:51 +0200

    [Decision.ml] Fixing the mess I caused

--- bin/main.ml
+++ bin/main.ml
@@ -1,15 +1,11 @@
 open Yojson.Safe
 open Mapgen
 open Dumpmap
+open Village
 open Decision
 
 let map = gen_map 400 8 10 2;;
 
-map |> serialize_map |> to_file "map1.json"
-
 let village_exp =
   (1, Vide, (stock_exp, needed_exp), (1, 1), [ (1, 1); (0, 0) ])
-    destroy_build (stock_exp, needed_exp)
-    [ (1, 1); (0, 0) ]
-    map map
-  |> serialize_map |> to_file "map2.json"
+
