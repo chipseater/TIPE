f20b24f76cbbefcd4f1d490979f3b922ed8e3f96
Astatre <chocolat.pourie@gmail.com>
2024-07-26 16:52:15 +0200

    [decision] too far from retirement

--- lib/decision.ml
+++ lib/decision.ml
@@ -3,6 +3,7 @@ open Mapmanage
 open Mapgen
 
 let shuffle arr =
+  Random.self_init ();
   let n = Array.length arr in
   for i = n - 1 downto 1 do
     let j = Random.int (i + 1) in
@@ -10,6 +11,12 @@ let shuffle arr =
     arr.(i) <- arr.(j);
     arr.(j) <- temp
   done
+
+                            (* OK *)
+
+;;
+
+
 
 let ingpercent r1 r2 ing x donnee : bool =
   let nr1 = search donnee r1 in
@@ -19,8 +26,11 @@ let ingpercent r1 r2 ing x donnee : bool =
       let dif = (nr1 - nr2) * 100 / nr1 in
       if nr1 > nr2 then dif > x else false
   | Less ->
-      let dif = (nr1 - nr2) * 100 / nr1 in
+      let dif = (nr2 - nr1) * 100 / nr2 in
       if nr1 < nr2 then dif > x else false
+  ;;
+
+(* OK *)
 
 let ingflat r1 r2 ing x donnee : bool =
   let nr1 = search donnee r1 in
@@ -31,7 +41,10 @@ let ingflat r1 r2 ing x donnee : bool =
       if nr1 > nr2 then dif > x else false
   | Less ->
       let dif = nr1 - nr2 in
-      if nr1 < nr2 then dif > x else false
+      if nr1 < nr2 then -dif > x else false
+  ;;
+
+(* OK *)
 
 let equalpercent r1 r2 x donnee =
   let nr1 = search donnee r1 in
@@ -40,13 +53,17 @@ let equalpercent r1 r2 x donnee =
   let som = nr1 + nr2 in
   dif * 100 / som < x
 
+;;
+
+(* OK *)
 let equalflat r1 r2 x donnee =
   let nr1 = search donnee r1 in
   let nr2 = search donnee r2 in
   let dif = nr1 - nr2 in
   let test = if dif < 0 then -dif else dif in
   test < x
-
+;;
+(* OK *)
 let test (donnee : data) (condition : condition) : bool =
   match condition with
   | Ingpercent (r1, r2, ing, x) -> ingpercent r1 r2 ing x donnee
@@ -54,18 +71,22 @@ let test (donnee : data) (condition : condition) : bool =
   | Equalflat (r1, r2, x) -> equalflat r1 r2 x donnee
   | Equalpercent (r1, r2, x) -> equalpercent r1 r2 x donnee
 
+(* Suppose *)
+
 let test_not_full (chunk : chunk) : bool =
   match chunk with
   | None -> failwith "izeovoap"
   | Chunk (x, _) ->
       let t = ref false in
-      for i = 0 to chunk_width do
-        for j = 0 to chunk_width do
+      for i = 0 to chunk_width-1 do
+        for j = 0 to chunk_width-1 do
           let (Tile (c, _)) = x.(i).(j) in
           if c == None then t := true
         done
       done;
       !t
+    ;;
+    (* OK *)
 
 let possibilite chunk =
   let arr = Array.make 16 (-1, -1) in
@@ -73,14 +94,16 @@ let possibilite chunk =
   | None -> failwith "izviuaebiazvbaui"
   | Chunk (tab, _) ->
       (let c = ref 0 in
-       for i = 0 to chunk_width do
-         for j = 0 to chunk_width do
+       for i = 0 to chunk_width-1 do
+         for j = 0 to chunk_width-1 do
            let (Tile (b, _)) = tab.(i).(j) in
            if b == None then arr.(!c) <- (i, j);
            c := !c + 1
          done
        done);
       arr
+
+(* OK *)
 
 let buildtile (build : building) (map : map) (table : (int * int) array) =
   shuffle table;
@@ -94,6 +117,8 @@ let buildtile (build : building) (map : map) (table : (int * int) array) =
     | i, j -> mutate_building_in_chunk map.(x).(y) (Some build) i j
   in
   choice arr 0
+(* OK *)
+
 
 let pos_card (pos_list : position list) =
   match pos_list with
@@ -106,7 +131,7 @@ let pos_card (pos_list : position list) =
         match pos_list with
         | [] ->
             let b, d, e, g = (!left, !right, !top, !bot) in
-            ((e + 1, b + 1), d - b, g - e)
+            ((e - 1, b - 1), d - b + 3, g - e + 3)
         | (a, b) :: q ->
             if a > !bot then bot := a;
             if a < !top then top := a;
@@ -115,6 +140,7 @@ let pos_card (pos_list : position list) =
             parc q
       in
       parc pos_list
+                              (* OK *)
 
 let rec proxi (arr : int array array) (pos_list : position list) (corner : position)
     =
@@ -132,21 +158,23 @@ let rec proxi (arr : int array array) (pos_list : position list) (corner : posit
       arr.(x + 1 - p).(y - m) <- arr.(x + 1 - p).(y - m) + 1;
       arr.(x + 1 - p).(y + 1 - m) <- arr.(x + 1 - p).(y + 1 - m) + 1 ; proxi arr q corner
 
-
+                              (* OK *)
 
 let parc_mat (arr : int array array) (h : int) (l : int) =
   let c = ref 0 in
   let list = ref [] in
   for i = 0 to h do
     for j = 0 to l do
-      if arr.(i).(j) > !c then list := [ (i, j) ]
+      if arr.(i).(j) > !c then (list := [ (i, j) ]; c := (arr.(i).(j)) ;)
       else if arr.(i).(j) = !c then list := (i, j) :: !list
       else ()
     done
   done;
   !list
 
-let buildout (build : building) (map : map) (pos_list : position list) : unit =
+                            (* OK *)
+
+let r_buildout (build : building) (map : map) (pos_list : position list) : unit =
   let coner, larg, haut = pos_card pos_list in
   let mat = Array.make_matrix (haut + 2) (larg + 2) 0 in
   proxi mat pos_list coner;
@@ -154,7 +182,7 @@ let buildout (build : building) (map : map) (pos_list : position list) : unit =
   let arr = Array.of_list list in
   buildtile build map arr
 
-let buildin (build : building) (map : map) (pos_list : position list) =
+let r_buildin (build : building) (map : map) (pos_list : position list) =
   let rec empile (pos_list : position list) : (int * int) list =
     match pos_list with
     | [] -> []
@@ -163,7 +191,7 @@ let buildin (build : building) (map : map) (pos_list : position list) =
   in
   let temp = empile pos_list in
   match temp with
-  | [] -> buildout build map pos_list
+  | [] -> r_buildout build map pos_list
   | _ :: _ ->
       let tab = Array.of_list temp in
       buildtile build map tab
@@ -172,9 +200,16 @@ let to_do (action : action) (map : map) (pos_list : position list) : unit =
   let arg, build, pref = action in
   if pref = Random then
     match arg with
-    | InCity -> buildin build map pos_list
-    | OutCity -> buildout build map pos_list
-  else ()
+    | InCity -> r_buildin build map pos_list
+    | OutCity -> r_buildout build map pos_list
+  else match pref with
+  | Pref(Forest) -> ()
+  | Pref(Desert) -> ()
+  | Pref(Plains) -> ()
+  |_ -> failwith("fnzqpvn")
+
+
+;;
 
 let rec eval_node (node : tree) (ressource : data) (pos_list : position list)
     (map : map) =
