ca470b550d1be3ee21e65337d31d0d2f73b66667
Astatre <chocolat.pourie@gmail.com>
2025-05-23 13:16:43 +0200

    pif 2

--- lib/variable.ml
+++ lib/variable.ml
@@ -14,6 +14,15 @@ let cout_scierie = [ (Bed, 0); (Nouriture, 0); (Main_d_oeuvre, 0); (Pierre, -20)
 
 let cout_ferme = [ (Bed, 0); (Nouriture, 0); (Main_d_oeuvre, -10); (Pierre, 0); (Wood, 0) ]
 
+let cout_statue = [ (Bed, 0); (Nouriture, 0); (Main_d_oeuvre, -20); (Pierre, -100); (Wood, 0) ]
+
+let cout_auberge = [ (Bed, 0); (Nouriture, 0); (Main_d_oeuvre, 0); (Pierre, 0); (Wood, -100) ]
+
+let cout_puit = [ (Bed, 0); (Nouriture, 0); (Main_d_oeuvre, 0); (Pierre, -50); (Wood, 0) ]
+
+let cout_vide = [ (Bed, 0); (Nouriture, 0); (Main_d_oeuvre, 0); (Pierre, 0); (Wood, 0) ]
+
+
 (* Un objet de type donne vide *)
 let void_donne : donne =
   [ (Bed, 0); (Nouriture, 0); (Main_d_oeuvre, 0); (Pierre, 0); (Wood, 0) ]
@@ -30,10 +39,26 @@ let ferme_donne_prodution : donne =
 
 let scierie_donne_prodution : donne =
   [ (Bed, 0); (Nouriture, 0); (Main_d_oeuvre, -10); (Pierre, 0); (Wood, 50) ]
+let puit_donne : donne =
+  [ (Bed, 0); (Nouriture, 0); (Main_d_oeuvre, 0); (Pierre, 0); (Wood, 0) ]
+
+let statue_donne : donne =
+  [ (Bed, 0); (Nouriture, 0); (Main_d_oeuvre, -10); (Pierre, 0); (Wood, 0) ]
+
+let auberge_donne : donne =
+  [ (Bed, 10); (Nouriture, -20); (Main_d_oeuvre, 0); (Pierre, 0); (Wood, 0) ]
 
 let void_donne : donne =
   [ (Bed, 0); (Nouriture, 0); (Main_d_oeuvre, 0); (Pierre, 0); (Wood, 0) ]
     
+  
+let ferme_modif b = 
+  let n = float_of_int b in 
+  [|1.;sqrt n;1.;1.;1.|]
+
+let puit_modif b =  let t = ( match b with |0 -> 1. |1 -> 1.75 |2 -> 2.5 |3 -> 1.2 |4 -> 0.5 |_ -> 0. ) in 
+  [|1.;t;1.;1.;1.|] 
+
 let void_modif = [|1.;1.;1.;1.;1.|] 
   
   
